af6a9a8b9642cc3afea7d02dcfe0a63a7b2dcb54
Girish-Yadav-devops <girishyadav007.0@gmail.com>
2025-01-03 17:19:42 +0530

    Intial Commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Junit from './Junit'; // Import your component
+
+function App() {
+  return (
+    <div className="App">
+      <Junit />  {/* Use your component here */}
+    </div>
+  );
+}
+
+export default App;
